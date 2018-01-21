a8ca44e3c843e617f0b791e979b72727c4af7d12
mao <mao20011221@gmail.com>
2018-01-21 16:06:39 +0900

    廊下、会話

--- app/src/main/java/net/kojira/a01_layouttest/MainActivity.java
+++ app/src/main/java/net/kojira/a01_layouttest/MainActivity.java
@@ -2,12 +2,42 @@ package net.kojira.a01_layouttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView text = findViewById(R.id.text);
+        final ImageView imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (count) {
+                    case 0:
+                        text.setText("uuuuuuuuuuuuuuuu");
+                        imageView.setImageResource(R.drawable.goriraen2);
+                        break;
+                    case 1:
+                        text.setText("eeeeeeeeeeeeee");
+                        break;
+                    case 2:
+                        text.setText("oooooooooooooo");
+                        break;
+                    case 3:
+                        text.setText("kkkkkkkkkkkkk");
+                        break;
+                }
+                count += 1;
+            }
+        });
+
     }
 }
